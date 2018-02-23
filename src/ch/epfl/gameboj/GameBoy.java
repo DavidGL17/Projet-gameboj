c4c6ae969c185c5798b7c77bfe708afaa7db759f
DavidGL17 <david.gonzalezleon@epfl.ch>
2018-02-23 14:57:44 +0100

    Squellette de la partie 2

---
+++ src/ch/epfl/gameboj/GameBoy.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package ch.epfl.gameboj;
+
+/**
+ * 
+ * @author David Gonzalez leon (270845)
+ * @author Melvin Malonga-Matouba (288405)
+ * 
+ */
+public class GameBoy {
+
+}
---
+++ src/ch/epfl/gameboj/component/cpu/Alu.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package ch.epfl.gameboj.component.cpu;
+
+/**
+ * 
+ * @author David Gonzalez leon (270845)
+ * @author Melvin Malonga-Matouba (288405)
+ * 
+ */
+public final class Alu {
+
+}
