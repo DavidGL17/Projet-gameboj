abb154cac3771832f92db02b4b870a5cfe85931f
DavidGL17 <david.gonzalezleon@epfl.ch>
2018-02-24 16:29:55 +0100

    GameBoy codée, non docummentée

--- src/ch/epfl/gameboj/GameBoy.java
+++ src/ch/epfl/gameboj/GameBoy.java
@@ -3,6 +3,9 @@
  */
 package ch.epfl.gameboj;
 
+import ch.epfl.gameboj.component.memory.Ram;
+import ch.epfl.gameboj.component.memory.RamController;
+
 /**
  * 
  * @author David Gonzalez leon (270845)
@@ -10,5 +13,21 @@ package ch.epfl.gameboj;
  * 
  */
 public class GameBoy {
+    private Bus bus;
+    private RamController workRam = new RamController(new Ram(AddressMap.WORK_RAM_SIZE), AddressMap.WORK_RAM_START, AddressMap.WORK_RAM_END);
+    private RamController echoRam = new RamController(new Ram(AddressMap.ECHO_RAM_SIZE), AddressMap.ECHO_RAM_START, AddressMap.ECHO_RAM_END);
+    
+    public GameBoy(Object cartridge) {
+        workRam.attachTo(bus);
+        echoRam.attachTo(bus);
+    }
 
+    /**
+     * Returns the gameBoy's Bus
+     * 
+     * @return bus The gameBoys bus
+     */
+    public Bus Bus() {
+        return bus;
+    }
 }
