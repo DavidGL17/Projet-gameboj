910f94334ec8a483bcbfff23fcd3c117106eac42
DavidGL17 <david.gonzalezleon@epfl.ch>
2018-04-02 17:22:46 +0200

    debug main passe, que deux test de blarrg qui échouent (le problème était dans bit_u3, j'ai mis des commentaires pour expliquer)

--- src/ch/epfl/gameboj/component/cpu/Cpu.java
+++ src/ch/epfl/gameboj/component/cpu/Cpu.java
@@ -498,7 +498,7 @@ public class Cpu implements Component, Clocked {
         } break;
 
         // Bit test and set
-        case BIT_U3_R8: {
+        case BIT_U3_R8: {//Enfaite z doit être à 1 si le bit vaut 0, ce qui est l'inverse de ce que return alu
 //            int value = Bits.extract(opcode.encoding, 3,3);
             Reg reg = extractReg(opcode,0);
             int flags = Alu.unpackFlags(Alu.testBit(Regs.get(reg), Bits.extract(opcode.encoding, 3, 3)));
