083e828dede499191c2aa49f76cc305917fdd034
Melvin <melvin.malonga-matouba@gmail.com>
2018-04-09 17:52:20 +0200

    BitVectorBuilder completed

--- src/ch/epfl/gameboj/bits/BitVector.java
+++ src/ch/epfl/gameboj/bits/BitVector.java
@@ -4,6 +4,7 @@
 package ch.epfl.gameboj.bits;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 import ch.epfl.gameboj.Preconditions;
 
@@ -11,6 +12,8 @@ import ch.epfl.gameboj.Preconditions;
  * @author David Gonzalez leon (270845)
  * @author Melvin Malonga-Matouba (288405)
  */
+
+//Juste pour être au clair, poids d'un bit croissant de l'index de l'entier dans lequel il se trouve 
 public final class BitVector {
     private int[] table;
     
@@ -24,5 +27,61 @@ public final class BitVector {
         this(size,false);
     }
     
+    
+    public static class Builder {
+    	
+    		private byte[] table;
+    		
+    		/**
+    		 * Builds a Builder 
+    		 * @param size the number of Bit of the desired BitVector
+    		 */
+    		public Builder(int size) {
+    			Preconditions.checkArgument(size>=0&&size%32==0);
+    			table = new byte[Math.floorDiv(size,8)];
+    		}
+    		
+    		/**
+    		 * Sets the byte of weight index to a value
+    		 * @param index 
+    		 * @param value
+    		 * @return the updated Builder
+    		 */
+    		public Builder setByte(int index, byte value) {
+    			if (table == null) {
+    				throw new IllegalStateException();
+    			}
+    			Objects.checkIndex(index, table.length);
+    			table[index]=value;
+    			
+    			return this;
+    		}
+    		
+    		/**
+    		 * Builds the BitVector corresponding to the Builder
+    		 * @return the BitVector
+    		 */
+    		public BitVector build(){
+    			//If the Builder hasn't been constructed throw NullPointerException
+    			int [] argument = buildIntTable();
+    			table=null;
+    			return new BitVector(argument);
+    		}
+    		
+    		/**
+    		 * Computes the int Array containing the same bits as table;
+    		 * @return the int array
+    		 */
+    		private int[] buildIntTable() {
+    			int[] res = new int [table.length/4];
+    			for (int rank=0 ; rank<res.length; rank++) {
+    				res[rank]= table[rank*4] + (table[rank*4+1] +(table[rank*4+2] + (table[rank*4+3] <<8) <<8 )<<8) ;
+    			}
+    			return res;
+    		}
+    	
+    	
+    	
+    }
     
 }
