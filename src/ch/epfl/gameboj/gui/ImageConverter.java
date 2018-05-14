f917fb8893193a0a0b0a84b7df4e0835c07d9d85
Melvin <melvin.malonga-matouba@gmail.com>
2018-05-14 20:44:02 +0200

    Revert "Correction des constantes"
    This reverts commit 6bec673b2595db6a60ef35d6e49f4bdeacbeb39f.

--- src/ch/epfl/gameboj/gui/ImageConverter.java
+++ src/ch/epfl/gameboj/gui/ImageConverter.java
@@ -8,7 +8,7 @@ import javafx.scene.image.WritableImage;
 
 public class ImageConverter {
 
-    private static final int[] MAP_COLORS = new int[] {0xFFFFFFFF, 0xD3D3D3D3, 0xA9A9A9A9, 0x00000000};
+    private static final int[] MAP_COLORS = new int[] {0xFFFFFFFF, 0xFFD3D3D3, 0xFFA9A9A9, 0xFF000000};
     
     public static Image convert(LcdImage image) {
         WritableImage finalImage = new WritableImage(LcdController.LCD_WIDTH, LcdController.LCD_HEIGHT);
