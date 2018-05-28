7202a6cf24570770b69a985b4bc9649a861e6aac
DavidGL17 <david.gonzalezleon@epfl.ch>
2018-05-28 14:17:09 +0200

    Mise au propre de ImageConverter

--- src/ch/epfl/gameboj/gui/ImageConverter.java
+++ src/ch/epfl/gameboj/gui/ImageConverter.java
@@ -8,19 +8,22 @@ import javafx.scene.image.WritableImage;
 
 public class ImageConverter {
 
-    private static final int[] MAP_COLORS = new int[] {0xFFFFFFFF, 0xFFD3D3D3, 0xFFA9A9A9, 0xFF000000};
-    
+    private static final int[] MAP_COLORS = new int[] { 0xFFFFFFFF, 0xFFD3D3D3,
+            0xFFA9A9A9, 0xFF000000 };
+
     /**
-     * Converts an LcdImage from the gameboy to a regular javafx Image
+     * Converts an LcdImage from the gameboy to a javafx Image
      * 
-     * @param image, an LcdImage
+     * @param image,
+     *            an LcdImage
      * @return the LcdImage converted to Image
      */
     public static Image convert(LcdImage image) {
-        WritableImage finalImage = new WritableImage(LcdController.LCD_WIDTH, LcdController.LCD_HEIGHT);
+        WritableImage finalImage = new WritableImage(LcdController.LCD_WIDTH,
+                LcdController.LCD_HEIGHT);
         PixelWriter pixelWriter = finalImage.getPixelWriter();
-        for (int y = 0;y<finalImage.getHeight();++y) {
-            for (int x = 0;x<finalImage.getWidth();++x) {
+        for (int y = 0; y < finalImage.getHeight(); ++y) {
+            for (int x = 0; x < finalImage.getWidth(); ++x) {
                 pixelWriter.setArgb(x, y, MAP_COLORS[image.get(x, y)]);
             }
         }
